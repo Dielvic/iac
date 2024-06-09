52a5e84f46276458923b724cf3a104fb8801e4b5
dielvic-kombila <dielvickombila@gmail.com>
2024-06-09 13:02:34 +0000

    fix ami id for archi 64_86 to arm

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-037504265c88c5c66" // Replace with the latest AMI ID for your region
+  default     = "ami-0ac08e87e03533a1d" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
